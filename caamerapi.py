b2b5575f5ff4e7fa679a96a7666c29ad77cd6005
AIIexTV <107073054+AIIexTV@users.noreply.github.com>
2023-07-11 20:04:55 +0200

    Create caamerapi.py

---
+++ caamerapi.py
@@ -0,0 +1,40 @@
+from flask import Flask, render_template, Response
+from picamera import PiCamera
+import time
+
+app = Flask(__name__)
+
+# Erstellen Sie eine Instanz der Kamera
+camera = PiCamera()
+
+
+def generate_frames():
+    while True:
+        # Legen Sie die Auflösung und andere Kameraeinstellungen fest
+        camera.resolution = (640, 480)
+        camera.rotation = 180
+
+        # Erstellen Sie einen Speicherpuffer für das Bild
+        frame = bytearray()
+
+        # Nehmen Sie ein Bild auf und speichern Sie es im Speicherpuffer
+        camera.capture(frame, 'jpeg')
+
+        # Geben Sie das Bild im MJPEG-Format zurück
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + bytes(frame) + b'\r\n\r\n')
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(generate_frames(),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
